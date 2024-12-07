6dff3a29d96e7defbc951a45d150d40c5cd98a40
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2024-12-07 22:23:27 +0700

    feat: suspense loader for home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,23 +1,12 @@
 import { Suspense } from "react";
 
 import SetUpScene from "./_components/SetUpScene";
+import LoadingPage from "../loading";
 
 const HomePage = () => {
   return (
     <main className="h-screen w-full">
-      <Suspense
-        fallback={
-          <section className="pointer-events-none fixed z-40 flex h-screen w-full flex-col items-center justify-center">
-            <p>Playground</p>
-            <h1 className="text-center text-5xl font-bold tracking-wider md:text-9xl">
-              {"Fadhil Kholaf".split("").map((item, index) => (
-                <span key={index}>{item}</span>
-              ))}
-            </h1>
-            <p>Experiment</p>
-          </section>
-        }
-      >
+      <Suspense fallback={<LoadingPage />}>
         <section className="pointer-events-none fixed z-40 flex h-screen w-full flex-col items-center justify-center">
           <p>Playground</p>
           <h1 className="text-center text-5xl font-bold tracking-wider md:text-9xl">
